912c0931c877c070597c4fb54079cf2c56ea83f1
AndreVictordmr <andre.victordmr@gmail.com>
2025-09-30 14:31:46 -0300

    LInkando fornecedores

---
+++ fornecedores/lista.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=], initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Listar Fornecedores</title>
+    </head>
+    <body>
+        <h1>Fornecedores</h1>
+        <a href="">Novo fornecedor</a>
+        <a href="../index.php">⬅️Voltar</a>
+    </body>
+</html>
